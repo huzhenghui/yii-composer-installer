8dacaed135337ead321b11bf2726568fa14fb7f6
胡争辉 <hu@daonao.com>
2021-06-27 11:45:29 +0800

    add postUpdate and postInstall

--- src/Installer.php
+++ src/Installer.php
@@ -1,19 +1,37 @@
 <?php
 
-namespace huzhenghui\yii\composer;
+namespace huzhenghui\yii\composer\installer;
 
 use Composer\Installer\LibraryInstaller;
 use Composer\Script\Event;
 
-use yii\composer\Install;
+use yii\composer\Installer as YiiComposerInstaller;
 
 class Installer extends LibraryInstaller
 {
+
+    public static function postUpdate($event)
+    {
+        YiiComposerInstaller::copyFiles(array(
+            "./vendor/yiisoft/yii2-app-basic/yii" => "./yii",
+            "./vendor/yiisoft/yii2-app-basic/config/console.php" => "./config/console.php",
+            "./vendor/yiisoft/yii2-app-basic/config/db.php" => "./config/db.php",
+            "./vendor/yiisoft/yii2-app-basic/config/params.php" => "./config/params.php",
+            "./vendor/yiisoft/yii2-app-basic/web/index.php" => "./web/index.php",
+            "./vendor/yiisoft/yii2-app-basic/config/web.php" => "./config/web.php",
+            "./vendor/yiisoft/yii2-app-basic/controllers/SiteController.php" => "./controllers/SiteController.php",
+            "./vendor/yiisoft/yii2-app-basic/views/site/index.php" => "./views/site/index.php",
+            "./vendor/yiisoft/yii2-app-basic/views/layouts/main.php" => "./views/layouts/main.php",
+            "./vendor/yiisoft/yii2-app-basic/assets/AppAsset.php" => "./assets/AppAsset.php",
+            "./vendor/yiisoft/yii2-app-basic/web/assets/.gitignore" => "./web/assets/.gitignore",
+            "./vendor/yiisoft/yii2-app-basic/widgets/Alert.php" => "./widgets/Alert.php"
+        ));
+    }
     /**
      * @param Event $event
      */
     public static function postInstall($event)
     {
-        yii\composer\Install::postInstall($event);
+        YiiComposerInstaller::generateCookieValidationKey("config/web.php");
     }
 }
